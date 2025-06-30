8636e7054a744914df74edc60b9163bc19edcd0a
Davaakhuu Enkhzaya <davaakhuuenkhzaya@Davaakhuus-MacBook-Air.local>
2025-06-30 10:17:08 +0800

    ki

--- BE-food-delivery/controller/food/add-food.ts
+++ BE-food-delivery/controller/food/add-food.ts
@@ -4,21 +4,28 @@ import { FoodModel } from "../../model/food.model";
 
 export const addFood = async (req: Request, res: Response) => {
   try {
-    const { foodName, price, image, ingredients, categoryName, category } =
-      req.body;
-    const isCategoryExisting = await FoodCategoryModel.findOne({
-      categoryName,
+    const { foodName, price, image, ingredients, category } = req.body;
+
+    const foodCategory = await FoodCategoryModel.findOne({
+      categoryName: category,
     });
+    if (!foodCategory) {
+      res.status(401).send("Category not found");
+      return;
+    }
+
     await FoodModel.create({
       foodName,
       price,
       image,
       ingredients,
-      category,
+      category: foodCategory._id,
     });
     // category deer postman deerees getcategory deer irsen id-g oruulna
     res.send({ message: "Successfully added food" });
   } catch (err) {
+    console.log(err);
+
     res.status(401).send("Hoolnii ner dawhtsahgvi oruulna uu");
   }
   // await FoodModel.create({ foodName, price, image, ingredients, category });
